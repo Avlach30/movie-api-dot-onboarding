49bf224bf0e9215ff0a434747ac3f61009cd338d
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-08-04 16:19:59 +0700

    develop authorization middleware

---
+++ src/middleware/authorize.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthorizeMiddleware extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+}
