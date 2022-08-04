18b277b7839d34989b94b07552d2a77643095431
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-08-04 13:39:25 +0700

    configure main app file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const configService = app.get(ConfigService);
+
+  const port: number = configService.get<number>('PORT');
+
+  await app.listen(port, () => {
+    console.log('Database connected!');
+    console.log(`Server running on http://localhost:${port}`);
+  });
 }
 bootstrap();
