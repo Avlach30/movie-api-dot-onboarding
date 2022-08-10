4e3b7936e3f1540f54a8483ce3350bcaac636883
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-08-10 09:58:02 +0700

    update confinguration of typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,19 @@
-import { join } from 'path';
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
+import { AppConfig } from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_USER_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: [
-          join(__dirname, '../', 'dist', 'entities/**/*.entity{ .ts,.js}'),
-        ],
-        synchronize: false,
-        migrations: [join(__dirname, '../', 'dist', 'migrations/*{.ts,.js}')],
-      }),
-    }),
+    TypeOrmModule.forRootAsync(AppConfig),
     UserModule,
     MovieModule,
   ],
